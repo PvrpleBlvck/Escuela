2f351e0bfd6d1740ed83b726d563388c0f4bc924
Norest Nyamhunga <41530377+PvrpleBlvck@users.noreply.github.com>
2021-12-09 18:00:35 +0200

    Update git.py

--- git.py
+++ git.py
@@ -1,1 +1,16 @@
+Pull requests
 
+Git init
+Git pull *clone or download URL*
+
+Git status
+#For Git to track that file, the add command is given. If you know the exact name of the file, you can specify that simply type the following command:
+Git add .
+
+Git commit -m “changes made”
+
+Git status
+Git remote add origin *URL*
+Git remote -v
+Git push -u origin master
+Git push -u origin master
