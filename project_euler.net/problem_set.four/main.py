d93e21a9e971c97731ef54f9ea82fbe1cfefbda3
Norest Nyamhunga <41530377+PvrpleBlvck@users.noreply.github.com>
2021-12-12 04:22:02 +0200

    Create main.py

---
+++ project_euler.net/problem_set.four/main.py
@@ -0,0 +1,10 @@
+def pvrle():
+	ans = max(p * q
+		for p in range(100, 1000)
+		for q in range(100, 1000)
+		if str(p * q) == str(p * q)[ : : -1])
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(pvrple())
