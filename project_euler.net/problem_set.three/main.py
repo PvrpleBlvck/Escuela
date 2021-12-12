2f23f51d443897aa60ff901d9a7d0474e680ba9b
Norest Nyamhunga <41530377+PvrpleBlvck@users.noreply.github.com>
2021-12-12 04:16:52 +0200

    Create main.py

---
+++ project_euler.net/problem_set.three/main.py
@@ -0,0 +1,7 @@
+def Biggest_Prime_Factor(n):
+    return next(n // i for i in range(1, n) if n % i == 0 and is_prime(n // i))
+  
+def is_prime(m):
+    return all(m % i for i in range(2, m - 1))
+  
+print(Biggest_Prime_Factor(600851475143))
